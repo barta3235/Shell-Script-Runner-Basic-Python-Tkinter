9068fe282af5cb20c2f32376949bb8b5a1d3ef43
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-08-03 07:54:28 +0600

    making objects more refined

--- Shell.py
+++ Shell.py
@@ -1,20 +1,10 @@
 
 import tkinter
-from tkinter import messagebox
 import subprocess
 from tkinter import filedialog
 
 class Shell_runner:
 
-    def shell_script(self,checker):
-        if checker == 0:
-            messagebox.showwarning(title="Warning", message='Please check the box to run shell scripts')
-        elif checker == 1:
-            print("Checking")
-            file_path = filedialog.askopenfile(title="Select a file",filetypes=[("Shell Scripts", "*.sh")])
+    def run_shell_script(self,file):
+        print(file)
 
-
-
-
-        else:
-            return
--- Test.py
+++ Test.py
@@ -1,13 +1,14 @@
 from Shell import Shell_runner
 import tkinter
-
+from tkinter import messagebox
+from tkinter import filedialog
 
 class Application:
     def __init__(self):
         self.mainWindow=tkinter.Tk()
 
 
-        # creating instance of the pther class
+        # creating instance of the other class
         self.shell_runner = Shell_runner()
 
 
@@ -22,7 +23,8 @@ class Application:
         self.checkSate = tkinter.IntVar()
 
         # button, passing shell_script function as a parameter
-        self.button = tkinter.Button(self.mainWindow, text='Upload Script', font=('Arial', 14),command=lambda : self.shell_runner.shell_script(self.checkSate.get()))
+        # self.button = tkinter.Button(self.mainWindow, text='Upload Script', font=('Arial', 14),command=lambda : self.shell_runner.shell_script(self.checkSate.get()))
+        self.button = tkinter.Button(self.mainWindow, text='Upload Script', font=('Arial', 14),command= lambda : self.run_command(self.checkSate.get()))
         self.button.pack(padx=50, pady=10)
 
 
@@ -32,6 +34,24 @@ class Application:
 
         # Opening the window
         self.mainWindow.mainloop()
+
+    # run script
+
+    def run_command(self,checker):
+        if checker == 0:
+            messagebox.showwarning(title="Warning", message='Please check the box to run shell scripts')
+
+        elif checker == 1:
+            print("Checking")
+
+            # selecting file from local pc
+            file_path = filedialog.askopenfile(title="Select a file", filetypes=[("Shell Scripts", "*.sh")])
+            self.shell_runner.run_shell_script(file_path)
+
+        else:
+            return
+
+
 
 
 if __name__ == '__main__':
