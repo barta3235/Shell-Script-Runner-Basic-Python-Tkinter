1f71b1d0861351e7975349bc26d2c49123406989
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-08-03 07:23:56 +0600

    subprocess

--- Shell.py
+++ Shell.py
@@ -1,6 +1,8 @@
 
 import tkinter
 from tkinter import messagebox
+import subprocess
+from tkinter import filedialog
 
 class Shell_runner:
 
@@ -8,6 +10,11 @@ class Shell_runner:
         if checker == 0:
             messagebox.showwarning(title="Warning", message='Please check the box to run shell scripts')
         elif checker == 1:
-            print('Here is your script')
+            print("Checking")
+            file_path = filedialog.askopenfile(title="Select a file",filetypes=[("Shell Scripts", "*.sh")])
+
+
+
+
         else:
             return
