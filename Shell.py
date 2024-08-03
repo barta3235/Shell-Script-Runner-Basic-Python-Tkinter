1d9a3f627dda1380c9251e97ea1e88b70dffa8b4
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-08-04 02:11:57 +0600

    v2- issue: permissions

--- Shell.py
+++ Shell.py
@@ -2,6 +2,7 @@ import os
 import tkinter
 import subprocess
 from tkinter import filedialog
+from tkinter import messagebox
 
 class Shell_runner:
 
@@ -12,13 +13,16 @@ class Shell_runner:
     def run_shell_script(self,file):
         print(file)
 
-       # making bash file executable if not
+        # making bash file executable if not
         if not os.access(file,os.X_OK):
             subprocess.call(['chmod','+x',file])
 
+        git_bash_path = r"C:\Program Files\Git\bin\bash.exe"
+
+
         #running the script on the output_text_widget
         try:
-            output= subprocess.run(['bash',file],check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,text=True)
+            output = subprocess.run([git_bash_path,file],check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True)
             # clearing output screen
             self.output_text_widget.delete(1.0, tkinter.END)
             self.output_text_widget.insert(tkinter.END,output.stdout)
